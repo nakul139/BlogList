50ec53a33060af459106a4f3c2e78143e350d15d
nakul139 <jainnakul543@gmail.com>
2024-10-20 17:00:41 +0000

    solution

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const BlogItem = props => {
+  const {blogDetail} = props
+  const {title, description, publishedDate} = blogDetail
+
+  return (
+    <li className="blog-item">
+      <div className="blog-container">
+        <h1 className="title-name">{title}</h1>
+        <p className="date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+    </li>
+  )
+}
+
+export default BlogItem
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      className="pfp-img"
+      alt="profile"
+    />
+    <h1 className="name">Wade Warren</h1>
+    <p className="description">Software Developer at UK</p>
+  </div>
+)
+
+export default UserInfo
